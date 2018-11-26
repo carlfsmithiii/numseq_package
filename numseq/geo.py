5c8b09388bd18b97f53fdae3bc39be7ce8cf4621
Carl Smith <carlfsmithiii@gmail.com>
2018-11-26 11:49:43 -0500

    implemented fib, geo, and prime modules in numseq package.  These modules are used in code.py in project root directory

---
+++ numseq/geo.py
@@ -0,0 +1,11 @@
+def square(num):
+    return num * num
+
+def triangle(num):
+    sum = 0
+    for i in range(num + 1):
+        sum += i
+    return sum
+
+def cube(num):
+    return num ** 3
---
+++ numseq/prime.py
@@ -0,0 +1,13 @@
+import math
+
+def primes(num):
+    """ Returns list of all primes <= num """
+    return [n for n in range(num + 1) if is_prime(n)]
+
+def is_prime(num):
+    if num % 2 == 0:
+        return False
+    for n in range(3, int(math.sqrt(num)) + 1, 2):
+        if num % n == 0:
+            return False
+    return True
